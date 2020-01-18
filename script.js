3519ac7b87fd106633b69e80e4e41975472c2677
Valentin <vgermanovich23@gmail.com>
2020-01-18 15:29:50 +0300

    Это мой первый коммит

---
+++ script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var leftBorderWidth = 1;
+let second =2;
+const pi = 3.14;
+
+console.log(second);
+
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+console.log(persone["name"]);
+
+
+let arr = ['plum.png','orange.jpeg','apple.bmp'];
+console.log(arr[2]);
+
+// alert("Hello World");
+/*let answer = confirm("Are u stupid?");
+console.log(answer);*/ 
+
+/*let answer = +prompt("Do u love to suck a dick?", "Yes");
+console.log(typeof(answer));*/
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr++);
+
+console.log("2" === 2);
+
+let isCheked = true,
+     isClose = false; 
+
+    Console.Log(isCheked || isClose);
+
+
+
+
+
